866a3cd7ea0be524f17e40ead1f4e000d94158b8
Soheil <soheil@DNa1c419d.SUNet>
2019-12-23 19:03:46 -0800

    updated unet - same input output resolution
    updated unet - same input output resolution

--- src/unet.py
+++ src/unet.py
@@ -51,7 +51,7 @@ class UNet(nn.Module):
 
         # NOTE: up_mode 'upsample' is incompatible with merging_mode 'add'
         if self.up_mode == 'upsample' and self.merging_mode == 'add':
-            raise ValueError("up_mode \"upsample\" is incompatible " "with merging_mode \"add\"")
+            raise ValueError("up_mode \"upsample\" is not compatible " "with merging_mode \"add\"")
 
         # creating the encoder path and adding them to a list
         for i in range(depth):
@@ -63,19 +63,11 @@ class UNet(nn.Module):
 
         # creating the decoder path and adding them to a list
         # NOTE: decoding only requires depth-1 blocks
-        for i in range(depth):
-
-            if i != depth-1:
-                ins = outs
-                outs = ins // 2
-                up_conv = UpConv(ins, outs, up_mode=up_mode, merging_mode=merging_mode)
-                self.up_convolutions.append(up_conv)
-            else:
-                ins = outs
-                outs = ins // 2
-                up_conv = UpConv(ins, outs, up_mode=up_mode, merging_mode="final")
-                self.up_convolutions.append(up_conv)
-
+        for i in range(depth-1):
+            ins = outs
+            outs = ins // 2
+            up_conv = UpConv(ins, outs, up_mode=up_mode, merging_mode=merging_mode)
+            self.up_convolutions.append(up_conv)
 
         self.conv_11 = conv11(outs, self.out_channels)
 
@@ -91,29 +83,25 @@ class UNet(nn.Module):
             init.constant(m.bias, 0)
 
     def reset_parameters(self):
+
         for i, m in enumerate(self.modules()):
             self.weight_initializaton(m)
 
     def forward(self, x):
-        encoder_outs = []
 
+        encoder_outs = []
         # encoder pathway, save outputs for merging
         for i, module in enumerate(self.down_convolutions):
             x, before_pool = module(x)
             encoder_outs.append(before_pool)
 
-        for i  in range(self.depth):
-
-            if i != self.depth-1:
-                module = self.up_convolutions[i]
-                before_pool = encoder_outs[-(i + 2)]
-                x = module(before_pool, x)
-            else:
-                module = self.up_convolutions[i]
-                before_pool = []
-                x = module(before_pool, x)
+        for i in range(self.depth-1):
+            module = self.up_convolutions[i]
+            before_pool = encoder_outs[-(i + 2)]
+            x = module(before_pool, x)
 
         x = self.conv_11(x)
+
         return x
 
 
@@ -121,26 +109,29 @@ if __name__ == "__main__":
 
     """
     TEST
+    args: 
+        out_channels (int): desired number of output channels that we want the output tensor have  
+        depth (int): the desired depth of the UNet
     """
 
+    # creating a pseudo-type input tensor of size batch_size*in_channels*x_dim*y_dim
     batch_size = 16
     in_channels, x_dim, y_dim = 8, 64, 64
     out_channels = 32
-
     input = np.random.random((batch_size, in_channels, x_dim, y_dim))
     input = Variable(torch.FloatTensor(input))
 
+    # running the UNet model for the given "depth" and desired number of "out_channels" on the input tensor
     model = UNet(out_channels=out_channels, in_channels=in_channels, depth=5, merging_mode='concat')
-    output = model(input)
+    model_output = model(input)
 
+    # checking the dimensions of the output and input
+    ## output dimension is: batch_size*out_channels*x_dim*y_dim
     print("low resolution input image shape:", input.shape)
-    print("high resolution output image shape:", output.shape)
+    print("high resolution output image shape:", model_output.shape)
 
     # criterion = nn.MSELoss()
     # loss = torch.sqrt(criterion(input, output[:,:8,:,:]))
     # loss.backward()
 
 
-## >> low resolution input image shape: torch.Size([16, 8, 64, 64])
-## >> high resolution output image shape: torch.Size([16, 32, 128, 128])
-## >> Process finished with exit code 0
