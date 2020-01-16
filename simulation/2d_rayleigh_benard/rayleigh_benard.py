29f017d3ec94a8c46c4f725c4a9682c6dd1b677d
Chiyu Jiang <maxjiang>
2020-01-16 13:56:31 -0800

    add argument parsing

--- simulation/2d_rayleigh_benard/rayleigh_benard.py
+++ simulation/2d_rayleigh_benard/rayleigh_benard.py
@@ -33,6 +33,7 @@ import numpy as np
 from mpi4py import MPI
 import time
 import pathlib
+import argparse
 
 from dedalus import public as de
 from dedalus.extras import flow_tools
@@ -41,110 +42,141 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-# Parameters
-Lx, Lz = (4., 1.)
-Prandtl = 1.
-Rayleigh = 1e6
-seed = 42
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Simulation script for Rayleigh-Benard 2D using Dedalus')
+    parser.add_argument('--lx', default=4.0, type=float,
+                        help='Physical length in x dimension. (default: 4.0)')
+    parser.add_argument('--lz', default=1.0, type=float,
+                        help='Physical length in z dimension. (default: 1.0)')
+    parser.add_argument('--res_x', default=512, type=int,
+                        help='Simulation resolution in x dimension. (default: 512)')
+    parser.add_argument('--res_z', default=128, type=int,
+                        help='Simulation resolution in z dimension. (default: 128)')
+    parser.add_argument('--dt', default=0.125, type=float,
+                        help='Simulation step size in time. (default: 0.125)')
+    parser.add_argument('--stop_sim_time', default=50., type=float,
+                        help='Simulation stop time. (default: 50)')
+    parser.add_argument('--rayleigh', default=1e6, type=float,
+                        help='Simulation Rayleigh number. (default: 1e6)')
+    parser.add_argument('--prandtl', default=1., type=float,
+                        help='Simulation Prandtl number. (default: 1.0)')
+    parser.add_argument('--seed', default=42, type=int,
+                        help='Random seed for initial perturbations. (default: 42)')
 
-# Create bases and domain
-x_basis = de.Fourier('x', 1024, interval=(0, Lx), dealias=3/2)  # 256
-z_basis = de.Chebyshev('z', 256, interval=(-Lz/2, Lz/2), dealias=3/2)  # 64
-domain = de.Domain([x_basis, z_basis], grid_dtype=np.float64)
+    args = parser.parse_args()
+    return args
 
-# 2D Boussinesq hydrodynamics
-problem = de.IVP(domain, variables=['p','b','u','w','bz','uz','wz'])
-problem.meta['p','b','u','w']['z']['dirichlet'] = True
-problem.parameters['P'] = (Rayleigh * Prandtl)**(-1/2)
-problem.parameters['R'] = (Rayleigh / Prandtl)**(-1/2)
-problem.parameters['F'] = F = 1
-problem.add_equation("dx(u) + wz = 0")
-# problem.add_equation("dt(b) - P*(dx(dx(b)) + dz(bz)) - F*w       = -(u*dx(b) + w*bz)")
-problem.add_equation("dt(b) - P*(dx(dx(b)) + dz(bz))             = -(u*dx(b) + w*bz)")
-problem.add_equation("dt(u) - R*(dx(dx(u)) + dz(uz)) + dx(p)     = -(u*dx(u) + w*uz)")
-problem.add_equation("dt(w) - R*(dx(dx(w)) + dz(wz)) + dz(p) - b = -(u*dx(w) + w*wz)")
-problem.add_equation("bz - dz(b) = 0")
-problem.add_equation("uz - dz(u) = 0")
-problem.add_equation("wz - dz(w) = 0")
-problem.add_bc("left(b) = 0.5")
-problem.add_bc("left(u) = 0")
-problem.add_bc("left(w) = 0")
-problem.add_bc("right(b) = -0.5")
-problem.add_bc("right(u) = 0")
-problem.add_bc("right(w) = 0", condition="(nx != 0)")
-problem.add_bc("right(p) = 0", condition="(nx == 0)")
 
-# Build solver
-solver = problem.build_solver(de.timesteppers.RK222)
-logger.info('Solver built')
+def main():
+    args = get_args()
+    # Parameters
+    Lx, Lz = (args.lx, args.lz)
+    Prandtl = args.prandtl
+    Rayleigh = args.rayleigh
+    seed = args.seed
 
-# Initial conditions or restart
-if not pathlib.Path('restart.h5').exists():
+    # Create bases and domain
+    x_basis = de.Fourier('x', args.res_x, interval=(0, Lx), dealias=3/2)  # 256
+    z_basis = de.Chebyshev('z', args.res_z, interval=(-Lz/2, Lz/2), dealias=3/2)  # 64
+    domain = de.Domain([x_basis, z_basis], grid_dtype=np.float64)
 
-    # Initial conditions
-    x = domain.grid(0)
-    z = domain.grid(1)
-    b = solver.state['b']
-    bz = solver.state['bz']
+    # 2D Boussinesq hydrodynamics
+    problem = de.IVP(domain, variables=['p','b','u','w','bz','uz','wz'])
+    problem.meta['p','b','u','w']['z']['dirichlet'] = True
+    problem.parameters['P'] = (Rayleigh * Prandtl)**(-1/2)
+    problem.parameters['R'] = (Rayleigh / Prandtl)**(-1/2)
+    problem.parameters['F'] = F = 1
+    problem.add_equation("dx(u) + wz = 0")
+    # problem.add_equation("dt(b) - P*(dx(dx(b)) + dz(bz)) - F*w       = -(u*dx(b) + w*bz)")
+    problem.add_equation("dt(b) - P*(dx(dx(b)) + dz(bz))             = -(u*dx(b) + w*bz)")
+    problem.add_equation("dt(u) - R*(dx(dx(u)) + dz(uz)) + dx(p)     = -(u*dx(u) + w*uz)")
+    problem.add_equation("dt(w) - R*(dx(dx(w)) + dz(wz)) + dz(p) - b = -(u*dx(w) + w*wz)")
+    problem.add_equation("bz - dz(b) = 0")
+    problem.add_equation("uz - dz(u) = 0")
+    problem.add_equation("wz - dz(w) = 0")
+    problem.add_bc("left(b) = 0.5")
+    problem.add_bc("left(u) = 0")
+    problem.add_bc("left(w) = 0")
+    problem.add_bc("right(b) = -0.5")
+    problem.add_bc("right(u) = 0")
+    problem.add_bc("right(w) = 0", condition="(nx != 0)")
+    problem.add_bc("right(p) = 0", condition="(nx == 0)")
 
-    # Random perturbations, initialized globally for same results in parallel
-    gshape = domain.dist.grid_layout.global_shape(scales=1)
-    slices = domain.dist.grid_layout.slices(scales=1)
-    rand = np.random.RandomState(seed=seed)
-    noise = rand.standard_normal(gshape)[slices]
+    # Build solver
+    solver = problem.build_solver(de.timesteppers.RK222)
+    logger.info('Solver built')
 
-    # Linear background + perturbations damped at walls
-    zb, zt = z_basis.interval
-    pert =  1e-3 * noise * (zt - z) * (z - zb)
-    b['g'] += F * pert
-    b.differentiate('z', out=bz)
+    # Initial conditions or restart
+    if not pathlib.Path('restart.h5').exists():
 
-    # Timestepping and output
-    dt = 0.125
-    stop_sim_time = 50
-    fh_mode = 'overwrite'
+        # Initial conditions
+        x = domain.grid(0)
+        z = domain.grid(1)
+        b = solver.state['b']
+        bz = solver.state['bz']
 
-else:
-    # Restart
-    write, last_dt = solver.load_state('restart.h5', -1)
+        # Random perturbations, initialized globally for same results in parallel
+        gshape = domain.dist.grid_layout.global_shape(scales=1)
+        slices = domain.dist.grid_layout.slices(scales=1)
+        rand = np.random.RandomState(seed=seed)
+        noise = rand.standard_normal(gshape)[slices]
 
-    # Timestepping and output
-    dt = last_dt
-    stop_sim_time = 50
-    fh_mode = 'append'
+        # Linear background + perturbations damped at walls
+        zb, zt = z_basis.interval
+        pert =  1e-3 * noise * (zt - z) * (z - zb)
+        b['g'] += F * pert
+        b.differentiate('z', out=bz)
 
-# Integration parameters
-solver.stop_sim_time = stop_sim_time
+        # Timestepping and output
+        dt = args.dt
+        stop_sim_time = args.stop_sim_time
+        fh_mode = 'overwrite'
 
-# Analysis
-snapshots = solver.evaluator.add_file_handler('snapshots', sim_dt=0.25, max_writes=50, mode=fh_mode)
-snapshots.add_system(solver.state)
+    else:
+        # Restart
+        write, last_dt = solver.load_state('restart.h5', -1)
 
-# CFL
-CFL = flow_tools.CFL(solver, initial_dt=dt, cadence=10, safety=1,
-                     max_change=1.5, min_change=0.5, max_dt=0.125, threshold=0.05)
-CFL.add_velocities(('u', 'w'))
+        # Timestepping and output
+        dt = last_dt
+        stop_sim_time = args.stop_sim_time
+        fh_mode = 'append'
 
-# Flow properties
-flow = flow_tools.GlobalFlowProperty(solver, cadence=10)
-flow.add_property("sqrt(u*u + w*w) / R", name='Re')
+    # Integration parameters
+    solver.stop_sim_time = stop_sim_time
 
-# Main loop
-try:
-    logger.info('Starting loop')
-    start_time = time.time()
-    while solver.proceed:
-        dt = CFL.compute_dt()
-        dt = solver.step(dt)
-        if (solver.iteration-1) % 10 == 0:
-            logger.info('Iteration: %i, Time: %e, dt: %e' %(solver.iteration, solver.sim_time, dt))
-            logger.info('Max Re = %f' %flow.max('Re'))
-except:
-    logger.error('Exception raised, triggering end of main loop.')
-    raise
-finally:
-    end_time = time.time()
-    logger.info('Iterations: %i' %solver.iteration)
-    logger.info('Sim end time: %f' %solver.sim_time)
-    logger.info('Run time: %.2f sec' %(end_time-start_time))
-    logger.info('Run time: %f cpu-hr' %((end_time-start_time)/60/60*domain.dist.comm_cart.size))
+    # Analysis
+    snapshots = solver.evaluator.add_file_handler('snapshots', sim_dt=0.25, max_writes=50, mode=fh_mode)
+    snapshots.add_system(solver.state)
+
+    # CFL
+    CFL = flow_tools.CFL(solver, initial_dt=dt, cadence=10, safety=1,
+                         max_change=1.5, min_change=0.5, max_dt=0.125, threshold=0.05)
+    CFL.add_velocities(('u', 'w'))
+
+    # Flow properties
+    flow = flow_tools.GlobalFlowProperty(solver, cadence=10)
+    flow.add_property("sqrt(u*u + w*w) / R", name='Re')
+
+    # Main loop
+    try:
+        logger.info('Starting loop')
+        start_time = time.time()
+        while solver.proceed:
+            dt = CFL.compute_dt()
+            dt = solver.step(dt)
+            if (solver.iteration-1) % 10 == 0:
+                logger.info('Iteration: %i, Time: %e, dt: %e' %(solver.iteration, solver.sim_time, dt))
+                logger.info('Max Re = %f' %flow.max('Re'))
+    except:
+        logger.error('Exception raised, triggering end of main loop.')
+        raise
+    finally:
+        end_time = time.time()
+        logger.info('Iterations: %i' %solver.iteration)
+        logger.info('Sim end time: %f' %solver.sim_time)
+        logger.info('Run time: %.2f sec' %(end_time-start_time))
+        logger.info('Run time: %f cpu-hr' %((end_time-start_time)/60/60*domain.dist.comm_cart.size))
+
+if __name__ == '__main__':
+    main()
