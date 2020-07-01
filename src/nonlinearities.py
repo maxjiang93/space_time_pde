864f3374ffb9115b002bcdc4ac6b06f5b2e70702
Chiyu Max Jiang <chiyu.jiang@berkeley.edu>
2020-07-01 10:49:58 -0700

    Update nonlinearities.py

--- src/nonlinearities.py
+++ src/nonlinearities.py
@@ -11,24 +11,12 @@ class Swish(nn.Module):
     def forward(self, x):
         return x * torch.sigmoid(self.beta * x)
 
-
-class Lambda(nn.Module):
-
-    def __init__(self, f):
-        super(Lambda, self).__init__()
-        self.f = f
-
-    def forward(self, x):
-        return self.f(x)
-        
         
 NONLINEARITIES = {
-    "tanh": nn.Tanh(),
-    "relu": nn.ReLU(),
-    "softplus": nn.Softplus(),
-    "elu": nn.ELU(),
-    "swish": Swish(),
-    "square": Lambda(lambda x: x**2),
-    "identity": Lambda(lambda x: x),
-    "leakyrelu": nn.LeakyReLU(),
+    "tanh": nn.Tanh,
+    "relu": nn.ReLU,
+    "softplus": nn.Softplus,
+    "elu": nn.ELU,
+    "swish": Swish,
+    "leakyrelu": nn.LeakyReLU,
 }
