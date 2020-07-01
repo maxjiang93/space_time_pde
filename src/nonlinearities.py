4c1b6f55b1a4627476041c3063f64eb103b5e5dd
Chiyu Max Jiang <chiyu.jiang@berkeley.edu>
2020-07-01 10:44:34 -0700

    Create nonlinearities.py
    Add various choices of nonlinearities to try.

---
+++ src/nonlinearities.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+
+
+class Swish(nn.Module):
+
+    def __init__(self):
+        super(Swish, self).__init__()
+        self.beta = nn.Parameter(torch.tensor(1.0))
+
+    def forward(self, x):
+        return x * torch.sigmoid(self.beta * x)
+
+
+class Lambda(nn.Module):
+
+    def __init__(self, f):
+        super(Lambda, self).__init__()
+        self.f = f
+
+    def forward(self, x):
+        return self.f(x)
+        
+        
+NONLINEARITIES = {
+    "tanh": nn.Tanh(),
+    "relu": nn.ReLU(),
+    "softplus": nn.Softplus(),
+    "elu": nn.ELU(),
+    "swish": Swish(),
+    "square": Lambda(lambda x: x**2),
+    "identity": Lambda(lambda x: x),
+    "leakyrelu": nn.LeakyReLU(),
+}
