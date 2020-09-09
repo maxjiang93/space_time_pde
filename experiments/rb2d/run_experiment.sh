1ffc83fea01014b990be184c58b758974461f2ca
Max Jiang <max.jiang@getcruise.com>
2020-09-08 19:32:11 -0700

    add choice of using continuity equations

--- experiments/rb2d/run_experiment.sh
+++ experiments/rb2d/run_experiment.sh
@@ -8,10 +8,11 @@ eval_dataset_name="rb2d_ra1e6_s42.npz"
 rayleigh=1000000
 prandtl=1
 gamma=0.0125
-log_dir_name="./log/Exp1/"
+use_continuity=true
+log_dir_name="./log/Exp1"
 mkdir -p $log_dir_name
 
 echo "[!] If you run into OOM error, try reducing batch_size_per_gpu or n_samp_pts_per_crop..."
-CUDA_VISIBLE_DEVICES=0 python train.py --epochs=100 --data_folder=data --log_dir=$log_dir_name --alpha_pde=$gamma --train_data=$train_dataset_name --eval_data=$eval_dataset_name --rayleigh=$rayleigh --prandtl=$prandtl --nonlin=softplus --batch_size_per_gpu=10 --n_samp_pts_per_crop=512
+CUDA_VISIBLE_DEVICES=0 python3 train.py --epochs=100 --data_folder=data --log_dir=$log_dir_name --alpha_pde=$gamma --train_data=$train_dataset_name --eval_data=$eval_dataset_name --rayleigh=$rayleigh --prandtl=$prandtl --nonlin=softplus --batch_size_per_gpu=10 --n_samp_pts_per_crop=512 --use_continuity=$use_continuity
 
 # please see the train.py for further tunable arguments during the training process
